85de203cd4c92e208ea9a2925a5a1edc3fb71381
Madimo <MadimoZhang@gmail.com>
2014-01-24 15:22:52 +0800

    finish ZHHotAnswers

---
+++ Zhihu/Library/ZhihuAPI/Models/ZHMAnswer.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Zhihu.Library.ZhihuAPI.Models
+{
+    public class ZHMAnswer
+    {
+        public ZHMAuthor author     = new ZHMAuthor();
+        public ZHMQuestion question = new ZHMQuestion();
+        public string url;
+        public string excerpt;
+        public string type;
+        public long updatedTime;
+        public int commentCount;
+        public int id;
+        public int voteupCount;
+
+        public void LoadFromJson(JObject answer)
+        {
+            if (answer["author"] != null)
+                author.LoadFromJson(answer["author"] as JObject);
+
+            if (answer["question"] != null)
+                question.LoadFromJson(answer["question"] as JObject);
+
+            if (answer["url"] != null)
+                this.url = (string)answer["url"];
+
+            if (answer["excerpt"] != null)
+                this.excerpt = (string)answer["excerpt"];
+
+            if (answer["type"] != null)
+                this.type = (string)answer["type"];
+
+            if (answer["updated_time"] != null)
+                this.updatedTime = (long)answer["updated_time"];
+
+            if (answer["comment_count"] != null)
+                this.commentCount = (int)answer["comment_count"];
+
+            if (answer["id"] != null)
+                this.id = (int)answer["id"];
+
+            if (answer["voteup_count"] != null)
+                this.voteupCount = (int)answer["voteup_count"];
+        }
+    }
+}
---
+++ Zhihu/Library/ZhihuAPI/ZHHotAnswers.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Zhihu.Library.ZhihuAPI.Models;
+
+namespace Zhihu.Library.ZhihuAPI
+{
+    public class ZHHotAnswers : ZhihuAPI
+    {
+        private string nextUrl;
+        private string previousUrl;
+
+        public List<ZHMAnswer> hotAnswers = new List<ZHMAnswer>();
+        public int pageNumber;
+
+        public async Task<int> GetFirstPage()
+        {
+            try
+            {
+                hotAnswers.Clear();
+
+                string result = await GetAsync("https://api.zhihu.com/explore/answers?hash=" + new Random().Next());
+                JObject json  = JObject.Parse(result);
+
+                nextUrl     = (string)json["paging"]["next"];
+                previousUrl = (string)json["paging"]["previous"];
+                pageNumber  = 1;
+
+                foreach (JObject item in (json["data"] as JArray))
+                {
+                    ZHMAnswer answer = new ZHMAnswer();
+                    answer.LoadFromJson(item);
+                    hotAnswers.Add(answer);
+                }
+
+                return hotAnswers.Count;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<int> GetNextPage()
+        {
+            try
+            {
+                hotAnswers.Clear();
+
+                string result = await GetAsync(nextUrl + "&hash=" + new Random().Next());
+                JObject json  = JObject.Parse(result);
+
+                nextUrl     = (string)json["paging"]["next"];
+                previousUrl = (string)json["paging"]["previous"];
+                pageNumber++;
+
+                foreach (JObject item in (json["data"] as JArray))
+                {
+                    ZHMAnswer answer = new ZHMAnswer();
+                    answer.LoadFromJson(item);
+                    hotAnswers.Add(answer);
+                }
+
+                return hotAnswers.Count;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
